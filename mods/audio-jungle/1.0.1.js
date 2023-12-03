0b0db0b5ec07b4b8d437296157ca394d9dfc5bcd
Brian Shao <brian@ngsh.ca>
2023-12-02 23:55:03 -0800

    repo: update audio-jungle@1.0.1

---
+++ mods/audio-jungle/1.0.1.js
@@ -0,0 +1,36 @@
+cML.define('audio-jungle', (mod) => {
+  const VIDEO =
+    'https://file.garden/ZWMT914ycxnRjtN7/Sburb%20Loading%20Screen.mp4';
+
+  mod.on('enable', () => {
+    env.loading = true;
+    body.classList.add('loading');
+
+    document.body.innerHTML += `
+      <div id="sbuban-container" style="position: absolute; top: 0; left: 0; width: 100%; height: 100%; z-index: 100000 !important; background-color: black;">
+        <button id="startbtn" style="top: 50%; left: 50%; width: 100%; height: 100%;  z-index: 100001 !important;">Start</button>
+        <video id="sbuban-jungle" style="display: none; top: 0; left: 0; width: 100%; height: 100%;">
+          <source src="${VIDEO}" type="video/mp4"></source>
+        </video>
+      </div>
+    `;
+
+    const button = document.querySelector('#startbtn');
+    button.onclick = () => {
+      const video = document.querySelector('#sbuban-jungle');
+
+      button.style.display = 'none';
+      video.style.display = '';
+
+      video.play().then(() => {
+        video.onended = () =>
+          document.body.removeChild(
+            document.querySelector('#sbuban-container'),
+          );
+      });
+
+      body.classList.remove('loading');
+      env.loading = false;
+    };
+  });
+});
